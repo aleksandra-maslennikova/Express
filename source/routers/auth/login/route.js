6b399e5a9c4228b2a3e14f6981ecde2141de3ae2
aleksandra-maslennikova <alex.maslennikova19@gmail.com>
2019-07-11 13:05:14 +0300

    fix eslint errors, fix previos homeworks

--- source/routers/auth/login/route.js
+++ source/routers/auth/login/route.js
@@ -1,10 +1,10 @@
-import { logError } from "../../../utils";
+import { logError } from '../../../utils';
 
 export const post = (req, res) => {
-  try {
-    res.status(204);
-  } catch (error) {
-    logError(error);
-    res.status(400).json({ message: "incorrect payload" });
-  }
+    try {
+        res.status(204);
+    } catch (error) {
+        logError(error);
+        res.status(400).json({ message: 'incorrect payload' });
+    }
 };
--- source/utils/validator.js
+++ source/utils/validator.js
@@ -1,17 +1,17 @@
 // Core
-import Ajv from "ajv";
-import { ValidationError } from "./errors";
+import Ajv from 'ajv';
+import { ValidationError } from './errors';
 
-export const validator = schema => (req, res, next) => {
-  const ajv = new Ajv({ allErrors: true });
-  const validate = ajv.compile(schema);
-  const valid = validate(req.body);
+export const validator = (schema) => (req, res, next) => {
+    const ajv = new Ajv({ allErrors: true });
+    const validate = ajv.compile(schema);
+    const valid = validate(req.body);
 
-  if (valid) {
-    return next();
-  }
+    if (valid) {
+        return next();
+    }
 
-  const errors = validate.errors.map(({ message }) => message).join(", ");
+    const errors = validate.errors.map(({ message }) => message).join(', ');
 
-  throw new ValidationError(errors, 400);
+    throw new ValidationError(errors, 400);
 };
